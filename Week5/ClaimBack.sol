46fb057dd5e091be1a91fc3f8f4ae4984920b5d6
Remix Workspace <>
2022-09-07 13:28:18 +0800

    a

--- Week5/ClaimBack.sol
+++ Week5/ClaimBack.sol
@@ -2,23 +2,30 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+error ClaimError(bytes);
+
 // 練習 https://docs.google.com/presentation/d/1-pXrW4Qaes4ZWNXoAio7OnhzTwg-dhgx6cSHxWVALKk/edit#slide=id.g15081281f2e_0_2
 contract ClaimBack {
-    address constant CONTRACT = 0xd33C69361e00f01C3085ac77ab2fA13bE10376E8;
+    address constant CONTRACT = payable(0xd33C69361e00f01C3085ac77ab2fA13bE10376E8);
     
     constructor() payable{}
 
-    function send() external payable {
-        payable(CONTRACT).transfer(1000000000000000);
+    function transfer() external payable returns(bytes memory) {
+        // payable(CONTRACT).transfer(1000000000000000);
+        (bool success, bytes memory data) = CONTRACT.call{value:1000000000000000}("");
+        if (!success) {
+            revert();
+        }
+        return data;
     }
 
     receive() external payable {
     }
     
-    function claim() external returns(bytes memory) {
-        (bool success, bytes memory data) = address(CONTRACT).call(abi.encodeWithSignature("clain(uint256)", 0));
+    function claim() external payable returns(bytes memory) {
+        (bool success, bytes memory data) = CONTRACT.call(abi.encodeWithSignature("claim(uint256)", 1));
         if (!success) {
-            revert();
+            revert ClaimError(data);
         }
         return data;
     }
