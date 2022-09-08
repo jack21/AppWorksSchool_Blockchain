4489fa78da9e4e55315de2b32cd61f2408b362f8
Remix Workspace <>
2022-09-08 23:27:06 +0800

    a

--- Week5/ArrayRemoveShift.sol
+++ Week5/ArrayRemoveShift.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Array1 {
+contract ArrayRemoveReplace {
     uint[] array = [1, 2, 3, 4, 5];
 
     // 取得 Array 內容(remove 後，驗證用)
@@ -13,10 +13,8 @@ contract Array1 {
     // 移除 array 內指定的 index（0-base）
     function remove(uint _index) external {
         require(_index < array.length);
-        // 從指定要移除的 index 開始 loop，複製後一個元素的內容值
-        for (uint i = _index; i < array.length - 1; i++) {
-            array[i] = array[i+1];
-        }
+        // 拿最後一個元素取代
+        array[_index] = array[array.length-1];
         // 移除 array 內最後一個元素
         array.pop();
     }
